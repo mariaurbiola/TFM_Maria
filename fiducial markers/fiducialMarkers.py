54e8d1039cf16c27d59fe7c0e16626236a615aba
María <urbiola.121411@e.unavarra.es>
2023-04-19 17:14:35 +0200

    Calibrate camera but not working

--- fiducial markers/fiducialMarkers.py
+++ fiducial markers/fiducialMarkers.py
@@ -8,13 +8,14 @@ sidePixels = 200
 borderBits = 1
 markerImage = cv.aruco.generateImageMarker(predefinedDictionary, id, sidePixels, borderBits)
 
-cv.imwrite("markerPredefined3.png", markerImage)
+cv.imwrite("markerPredefined"+str(id)+".png", markerImage)
 
 
 #Automatic dictionary
 numberOfMarkers = 10
 numberOfBits = 5
-automaticDictionary = cv.aruco.extendDictionary(numberOfMarkers, numberOfBits)
-markerImage = cv.aruco.generateImageMarker(automaticDictionary, id, sidePixels, borderBits)
+for i in range(numberOfMarkers):
+    automaticDictionary = cv.aruco.extendDictionary(numberOfMarkers, numberOfBits)
+    markerImage = cv.aruco.generateImageMarker(automaticDictionary, i, sidePixels, borderBits)
 
-cv.imwrite("markerAutomatic3.png", markerImage)
+    cv.imwrite("markerAutomatic"+str(i)+".png", markerImage)
